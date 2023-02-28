ee30c88202674762c418505feeb1ac2a55c49b49
Houston <roman.martseniuk.developer@gmail.com>
2023-02-28 20:44:53 +0200

    #

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const open_burger = document.querySelector('.open-burger');
 const close_burger = document.querySelector('.close-burger');
 const burger = document.querySelector('.header__nav');
 
-open_burger.onclick = function() {
+open_burger.onclick = function () {
    open_burger.classList.toggle('active');
    close_burger.classList.toggle('active');
    burger.classList.toggle('active');
@@ -42,3 +42,4 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.btn-left   ',
    },
 });
+
